0ee1ffbac99dc16e7de8072966d4b5b77363088a
luciasousa <luciamsousa00@ua.pt>
2023-06-13 11:58:06 +0100

    skills for computacional engineering

---
+++ main_stopwords.py
@@ -0,0 +1,228 @@
+#engenharia computacional (introdução e projeto) inglês e português
+
+import base64
+import re
+import zlib
+import pandas as pd
+from bs4 import BeautifulSoup
+import time
+from pprint import pprint
+import nltk
+nltk.download('punkt')
+import spacy
+from sklearn.feature_extraction.text import CountVectorizer
+import requests
+import json
+import textract
+import os
+
+# Define the ESCO API endpoint and language
+ESCO_API_ENDPOINT = "http://localhost:8080/search"
+ESCO_API_LANG_EN = "en"
+ESCO_API_LANG_PT = "pt"
+
+
+file = "../DPUC_2021_2022.xlsx"
+
+cols = ['CodUC', 'NomeUC', 'Ano', 'Semestre', 'Estado', 'Campo', 'TextoPT', 'TextoEN']
+
+db = pd.read_excel(file, usecols=cols)
+
+filtered_db_intro_pt = db.loc[db['NomeUC'].isin(['INTRODUÇÃO À ENGENHARIA COMPUTACIONAL'])].copy()
+filtered_db_intro_en = db.loc[db['NomeUC'].isin(['INTRODUÇÃO À ENGENHARIA COMPUTACIONAL'])].copy()
+filtered_db_proj_pt = db.loc[db['NomeUC'].isin(['PROJETO EM ENGENHARIA COMPUTACIONAL'])].copy()
+filtered_db_proj_en = db.loc[db['NomeUC'].isin(['PROJETO EM ENGENHARIA COMPUTACIONAL'])].copy()
+
+# Remove HTML tags and extract text content
+filtered_db_intro_pt.loc[:, 'TextoPT'] = filtered_db_intro_pt['TextoPT']
+filtered_db_intro_en.loc[:, 'TextoEN'] = filtered_db_intro_en['TextoEN']
+
+filtered_db_proj_pt.loc[:, 'TextoPT'] = filtered_db_proj_pt['TextoPT']
+filtered_db_proj_en.loc[:, 'TextoEN'] = filtered_db_proj_en['TextoEN']
+
+#remove stopwords
+stopwords_pt = nltk.corpus.stopwords.words('portuguese')
+stopwords_en = nltk.corpus.stopwords.words('english')
+
+text_tokens_intro_pt = [nltk.word_tokenize(text) for text in filtered_db_intro_pt['TextoPT'].tolist() if isinstance(text, str)]
+text_tokens_intro_en = [nltk.word_tokenize(text) for text in filtered_db_intro_en['TextoEN'].tolist() if isinstance(text, str)]
+text_tokens_proj_pt = [nltk.word_tokenize(text) for text in filtered_db_proj_pt['TextoPT'].tolist() if isinstance(text, str)]
+text_tokens_proj_en = [nltk.word_tokenize(text) for text in filtered_db_proj_en['TextoEN'].tolist() if isinstance(text, str)]
+
+#extract the sentences from the text
+sentences_intro_pt = [nltk.sent_tokenize(' '.join(tokens)) for tokens in text_tokens_intro_pt]
+sentences_intro_en = [nltk.sent_tokenize(' '.join(tokens)) for tokens in text_tokens_intro_en]
+sentences_proj_pt = [nltk.sent_tokenize(' '.join(tokens)) for tokens in text_tokens_proj_pt]
+sentences_proj_en = [nltk.sent_tokenize(' '.join(tokens)) for tokens in text_tokens_proj_en]
+
+#remove stopwords from the sentences
+
+'''
+sentences_intro_pt = []
+for sentence in sentences_intro_pt:
+    sentence_tokens = nltk.word_tokenize(str(sentence))
+    sentence = [word for word in sentence_tokens if not word in stopwords_pt]
+    sentences_intro_pt.append(sentence)
+
+
+sentences_intro_en = []
+for sentence in sentences_intro_en:
+    sentence_tokens = nltk.word_tokenize(str(sentence))
+    sentence = [word for word in sentence_tokens if not word in stopwords_en]
+    sentences_intro_en.append(sentence)
+
+
+sentences_proj_pt = []
+for sentence in sentences_proj_pt:
+    sentence_tokens = nltk.word_tokenize(str(sentence))
+    sentence = [word for word in sentence_tokens if not word in stopwords_pt]
+    sentences_proj_pt.append(sentence)
+
+
+sentences_proj_en = []
+for sentence in sentences_proj_en:
+    sentence_tokens = nltk.word_tokenize(str(sentence))
+    sentence = [word for word in sentence_tokens if not word in stopwords_en]
+    sentences_proj_en.append(sentence)
+'''
+
+skills = []
+sentence = ' '.join([re.sub(r'[^a-zA-Z\s]', '', str(s)) for s in sentences_intro_pt])
+sentence = ' '.join(dict.fromkeys(sentence.split()))
+firstpart, secondpart = sentence[:len(sentence)//2], sentence[len(sentence)//2:]
+print(sentence)
+
+response1 = requests.get(ESCO_API_ENDPOINT + "?text=" + firstpart + "&language=" + ESCO_API_LANG_PT +  "&type=skill&facet=type&facet=isInScheme&full=true&q=computer%20engineering")
+response2 = requests.get(ESCO_API_ENDPOINT + "?text=" + secondpart + "&language=" + ESCO_API_LANG_PT +  "&type=skill&facet=type&facet=isInScheme&full=true&q=computer%20engineering")
+
+# Check if the request was successful
+
+if response1.status_code != 200 and response2.status_code != 200:
+    print("Error fetching data from API")
+else:
+    # Extract the data from the response
+    data1 = response1.json()
+    data2 = response2.json()
+    # Print the matches and their positions in the text
+    #print("Sentence: " + sentence)
+    for match in data1["_embedded"]["results"]:
+        skill = match['preferredLabel'][ESCO_API_LANG_PT]
+        skills.append(skill)
+    for match in data2["_embedded"]["results"]:
+        skill = match['preferredLabel'][ESCO_API_LANG_PT]
+        skills.append(skill)
+        #print("ESCO: ",f"{match['preferredLabel']['en']}")
+
+#print(skills)
+with open("skills_engenharia_computacional_pt_sw/skills_introducao_pt_sw.txt", "w") as f:
+    f.write('\n'.join(skills))
+
+f.close()
+
+skills = []
+sentence = ' '.join([re.sub(r'[^a-zA-Z\s]', '', str(s)) for s in sentences_proj_pt])
+sentence = ' '.join(dict.fromkeys(sentence.split()))
+firstpart, secondpart = sentence[:len(sentence)//2], sentence[len(sentence)//2:]
+print(sentence)
+
+response1 = requests.get(ESCO_API_ENDPOINT + "?text=" + firstpart + "&language=" + ESCO_API_LANG_PT +  "&type=skill&facet=type&facet=isInScheme&full=true&q=computer%20engineering")
+response2 = requests.get(ESCO_API_ENDPOINT + "?text=" + secondpart + "&language=" + ESCO_API_LANG_PT +  "&type=skill&facet=type&facet=isInScheme&full=true&q=computer%20engineering")
+
+# Check if the request was successful
+
+if response1.status_code != 200 and response2.status_code != 200:
+    print("Error fetching data from API")
+else:
+    # Extract the data from the response
+    data1 = response1.json()
+    data2 = response2.json()
+    # Print the matches and their positions in the text
+    #print("Sentence: " + sentence)
+    for match in data1["_embedded"]["results"]:
+        skill = match['preferredLabel'][ESCO_API_LANG_PT]
+        skills.append(skill)
+    for match in data2["_embedded"]["results"]:
+        skill = match['preferredLabel'][ESCO_API_LANG_PT]
+        skills.append(skill)
+        #print("ESCO: ",f"{match['preferredLabel']['en']}")
+
+#print(skills)
+with open("skills_engenharia_computacional_pt_sw/skills_projeto_pt_sw.txt", "w") as f:
+    f.write('\n'.join(skills))
+
+f.close()
+
+
+skills = []
+sentence = ' '.join([re.sub(r'[^a-zA-Z\s]', '', str(s)) for s in sentences_intro_en])
+sentence = ' '.join(dict.fromkeys(sentence.split()))
+firstpart, secondpart = sentence[:len(sentence)//2], sentence[len(sentence)//2:]
+#compressed_data = zlib.compress(sentence.encode())
+# Get the compressed data as a string
+#sentence = base64.b64encode(compressed_data).decode()
+print(sentence)
+
+response1 = requests.get(ESCO_API_ENDPOINT + "?text=" + firstpart + "&language=" + ESCO_API_LANG_EN +  "&type=skill&facet=type&facet=isInScheme&full=true&q=computer%20engineering")
+response2 = requests.get(ESCO_API_ENDPOINT + "?text=" + secondpart + "&language=" + ESCO_API_LANG_EN +  "&type=skill&facet=type&facet=isInScheme&full=true&q=computer%20engineering")
+
+# Check if the request was successful
+
+if response1.status_code != 200 and response2.status_code != 200:
+    print("Error fetching data from API")
+else:
+    # Extract the data from the response
+    data1 = response1.json()
+    data2 = response2.json()
+    # Print the matches and their positions in the text
+    #print("Sentence: " + sentence)
+    for match in data1["_embedded"]["results"]:
+        skill = match['preferredLabel'][ESCO_API_LANG_EN]
+        skills.append(skill)
+    for match in data2["_embedded"]["results"]:
+        skill = match['preferredLabel'][ESCO_API_LANG_EN]
+        skills.append(skill)
+        #print("ESCO: ",f"{match['preferredLabel']['en']}")
+
+#print(skills)   
+with open("skills_engenharia_computacional_en_sw/skills_introducao_en_sw.txt", "w") as f:
+    f.write('\n'.join(skills))
+
+f.close()
+
+skills = []
+sentence = ' '.join([re.sub(r'[^a-zA-Z\s]', '', str(s)) for s in sentences_proj_en])
+sentence = ' '.join(dict.fromkeys(sentence.split()))
+firstpart, secondpart = sentence[:len(sentence)//2], sentence[len(sentence)//2:]
+#compressed_data = zlib.compress(sentence.encode())
+# Get the compressed data as a string
+#sentence = base64.b64encode(compressed_data).decode()
+print(sentence)
+
+response1 = requests.get(ESCO_API_ENDPOINT + "?text=" + firstpart + "&language=" + ESCO_API_LANG_EN +  "&type=skill&facet=type&facet=isInScheme&full=true&q=computer%20engineering")
+response2 = requests.get(ESCO_API_ENDPOINT + "?text=" + secondpart + "&language=" + ESCO_API_LANG_EN +  "&type=skill&facet=type&facet=isInScheme&full=true&q=computer%20engineering")
+
+# Check if the request was successful
+
+if response1.status_code != 200 and response2.status_code != 200:
+    print("Error fetching data from API")
+else:
+    # Extract the data from the response
+    data1 = response1.json()
+    data2 = response2.json()
+    # Print the matches and their positions in the text
+    #print("Sentence: " + sentence)
+    for match in data1["_embedded"]["results"]:
+        skill = match['preferredLabel'][ESCO_API_LANG_EN]
+        skills.append(skill)
+    for match in data2["_embedded"]["results"]:
+        skill = match['preferredLabel'][ESCO_API_LANG_EN]
+        skills.append(skill)
+        #print("ESCO: ",f"{match['preferredLabel']['en']}")
+
+#print(skills)        
+with open("skills_engenharia_computacional_en_sw/skills_projeto_en_sw.txt", "w") as f:
+    f.write('\n'.join(skills))
+
+f.close()
+
+
--- read_database.py
+++ read_database.py
@@ -1,9 +1,16 @@
-import pandas
+import pandas as pd
+from bs4 import BeautifulSoup
 
 file = "../DPUC_2021_2022.xlsx"
 
 cols = ['CodUC', 'NomeUC', 'Ano', 'Semestre', 'Estado', 'Campo', 'TextoPT', 'TextoEN']
 
-db = pandas.read_excel(file, usecols=cols)
+db = pd.read_excel(file, usecols=cols)
 
+filtered_db = db.loc[db['NomeUC'].isin(['INTRODUÇÃO À ENGENHARIA COMPUTACIONAL'])].copy()
 
+# Remove HTML tags and extract text content
+filtered_db.loc[:, 'TextoPT'] = filtered_db['TextoPT'].apply(lambda x: BeautifulSoup(x, 'html.parser').get_text() if isinstance(x, str) else x)
+filtered_db.loc[:, 'TextoEN'] = filtered_db['TextoEN'].apply(lambda x: BeautifulSoup(x, 'html.parser').get_text() if isinstance(x, str) else x)
+
+filtered_db.to_excel('introducao_eC.xlsx', index=False)
